5299bba45569f6d0510fd5a9b21fff7854e0fcd1
Vivek Tandel <vivektandel4@gmail.com>
2023-02-13 18:23:56 +0530

    chakra ui setup complete

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+/*
+To Extend the theme of the application
+import {extendTheme} from "@chakra-ui/react"
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+<ChakraProvider theme={theme}>
+  <Component {...pageProps} />
+</ChakraProvider>
+
+*/
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
